8a8088e05d8ca5843d81d4e89a8b7987f439ba54
Muskan <muskan.ved@mangoitsolutions.in>
2024-05-14 10:54:30 +0530

    package updation

--- resources/js/pages/success.vue
+++ resources/js/pages/success.vue
@@ -2,19 +2,19 @@
     <div class="container">
       <div class="row mt-4">
           <div class="col-lg-4 col-md-4 col-sm-12 col-12">
-            <img class="img-fluid for-light" src="/images/left-img.png" alt="">
+            <!-- <img class="img-fluid for-light" src="/images/left-img.png" alt=""> -->
           </div>
 
 
           <div class="col-lg-4 col-md-4 col-sm-12 col-12 text-center mt-4">
-            <img  class="img-fluid for-light d-flex float-right" src="/images/cap-img.png" alt="">
+            <!-- <img  class="img-fluid for-light d-flex float-right" src="/images/cap-img.png" alt=""> -->
               <h1>Congratulations!</h1>
               <p class="f-16">Your order has been placed successfully.</p>
               <a href="/" class="btn btn-primary mt-4 mb-5" style="color: #007bff !important; background-color: white;">Continue Shopping</a>
           </div>
           
           <div class="col-lg-4 col-md-4 col-sm-12 col-12">
-            <img class="img-fluid for-light" src="/images/right-img.png" alt="">
+            <!-- <img class="img-fluid for-light" src="/images/right-img.png" alt=""> -->
           </div>
         </div>
     </div>
